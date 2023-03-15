c75b5a47a63be70b1bd6d9b95c431fcf84047072
Dat Proto <dotatdat1997@hotmail.com>
2023-03-15 18:07:11 +0700

    DOING: Building atom components

---
+++ src/components/atoms/Input.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface IInput {
+  label?: string
+  content: {
+    type?: string
+    name?: string
+    placeholder?: string
+  }
+}
+function Input({ label, content = { type: 'text', name: 'name', placeholder: 'write something ...' } }: IInput) {
+  return (
+    <div className="flex flex-col gap-1">
+      {label && <label htmlFor="name" className="text-sm text-form-denim">{label}</label>}
+      <input type={content.type} name={content.name} placeholder={content.placeholder}
+             className="rounded border border-form-gray-light px-4 py-3 font-form font-medium capitalize text-form-gray-normal"/>
+    </div>
+  )
+}
+
+export default Input
