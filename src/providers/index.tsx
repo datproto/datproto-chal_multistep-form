795c040b7c7bc066978a198dbc953cdef82b70b6
Dat Proto <contact@datproto.com>
2023-04-02 13:50:30 +0700

    UPDATE: Fix eslint

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,11 @@
 'use client'
 
+import { AnimatePresence } from 'framer-motion'
 import { PropsWithChildren, useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
-import store from '@/store'
-import { AnimatePresence } from 'framer-motion'
-
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import store from '@/store'
 
 export default function Providers({ children }: PropsWithChildren) {
   const [render, setRender] = useState(false)
